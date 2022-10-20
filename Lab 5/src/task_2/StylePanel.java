5867cd2907f74a2179f8cf64130c5b5a5bc41477
Abdulkarim Koshak <15035598+MrGriefer@users.noreply.github.com>
2022-10-20 22:18:42 +0300

    Create StylePanel.java

---
+++ Lab 5/src/task_2/StylePanel.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+/**
+ * The StylePanel class allows the user to select the
+ * style of window shade from a combo box.
+ */
+public class StylePanel extends JPanel {
+
+    // The following constants are used to indicate
+    // the cost of each shade styles.
+    public final int REGULAR_SHADES = 0;
+    public final int FOLDING_SHADES = 10;
+    public final int ROMAN_SHADES = 15;
+
+    private JComboBox<String> comboBox; // A list of shade styles
+
+    // The following array holds the values that will
+    // be displayed in the combo box.
+    private String[] styles = {"Regular shades", "Folding shades", "Roman shades"};
+
+    /**
+     * Constructor
+     */
+    public StylePanel() {
+        // Create the combo box.
+        comboBox = new JComboBox<>(styles);
+
+        // Add a border around the panel.
+        setBorder(BorderFactory.createTitledBorder("Shade Style"));
+
+        // Add the combo box to the panel.
+        add(comboBox);
+    }
+
+    /**
+     * getStyleCost method
+     * @return The cost of the selected shade style.
+     */
+    public int getStyleCost() {
+        int styleCost = REGULAR_SHADES;
+
+        int index = comboBox.getSelectedIndex();
+        switch (index) {
+            case 1 -> styleCost = FOLDING_SHADES;
+            case 2 -> styleCost = ROMAN_SHADES;
+        }
+
+        return styleCost;
+    }
+}
